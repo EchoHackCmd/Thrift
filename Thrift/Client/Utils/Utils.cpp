99193ec482dde5381f7fcb16377ff0df5ab07830
EchoHackCmd <EchoHackCmd@gmail.com>
2022-10-30 13:34:27 +0000

    Update Utils.cpp

--- Thrift/Client/Utils/Utils.cpp
+++ Thrift/Client/Utils/Utils.cpp
@@ -126,3 +126,120 @@ auto Utils::debugOutput(std::string output) -> void {
 	return fStream.close();
 
 };
+
+auto Utils::ColorConvertRGBtoHSV(float r, float g, float b, float& out_h, float& out_s, float& out_v) -> void {
+
+	float K = 0.f;
+	if (g < b) {
+		auto tmp = g;
+		g = b;
+		b = tmp;
+
+		K = -1.f;
+	}
+	if (r < g) {
+		auto tmp = r;
+		r = g;
+		g = tmp;
+
+		K = -2.f / 6.f - K;
+	}
+
+	const float chroma = r - (g < b ? g : b);
+	out_h = fabsf(K + (g - b) / (6.f * chroma + 1e-20f));
+	out_s = chroma / (r + 1e-20f);
+	out_v = r;
+
+};
+
+auto Utils::ColorConvertHSVtoRGB(float h, float s, float v, float& out_r, float& out_g, float& out_b) -> void {
+
+	if (s == 0.0f) {
+		// gray
+		out_r = out_g = out_b = v;
+		return;
+	}
+
+	h = fmodf(h, 1.0f) / (60.0f / 360.0f);
+	int i = (int)h;
+	float f = h - (float)i;
+	float p = v * (1.0f - s);
+	float q = v * (1.0f - s * f);
+	float t = v * (1.0f - s * (1.0f - f));
+
+	switch (i) {
+		
+		case 0:
+			
+			out_r = v;
+			out_g = t;
+			out_b = p;
+
+		break;
+	
+		case 1:
+			
+			out_r = q;
+			out_g = v;
+			out_b = p;
+
+		break;
+	
+		case 2:
+			
+			out_r = p;
+			out_g = v;
+			out_b = t;
+
+		break;
+		
+		case 3:
+			
+			out_r = p;
+			out_g = q;
+			out_b = v;
+
+		break;
+		
+		case 4:
+			
+			out_r = t;
+			out_g = p;
+			out_b = v;
+
+		break;
+	
+		case 5:
+	
+		default:
+			
+			out_r = v;
+			out_g = p;
+			out_b = q;
+		
+		break;
+	}
+
+};
+
+auto Utils::ApplyRainbow(float* rcolors, float modifier = 0.003f) -> void {
+
+	if (rcolors[3] < 1) {
+		
+		rcolors[0] = 1;
+		rcolors[1] = 0.6f;
+		rcolors[2] = 0.6f;
+		rcolors[3] = 1;
+
+	};
+
+	Utils::ColorConvertRGBtoHSV(rcolors[0], rcolors[1], rcolors[2], rcolors[0], rcolors[1], rcolors[2]);
+
+	rcolors[0] += modifier;
+	
+	if (rcolors[0] >= 1)
+		rcolors[0] = 0;
+
+	Utils::ColorConvertHSVtoRGB(rcolors[0], rcolors[1], rcolors[2], rcolors[0], rcolors[1], rcolors[2]);
+
+};
