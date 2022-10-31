0c9f843fd3e988da622f3478e6c27862dbdf3992
EchoHackCmd <EchoHackCmd@gmail.com>
2022-10-31 00:10:06 +0000

    Update ClientInstance.cpp

--- Thrift/Client/SDK/Classes/ClientInstance.cpp
+++ Thrift/Client/SDK/Classes/ClientInstance.cpp
@@ -1,5 +1,11 @@
 #include "ClientInstance.h"
 
+auto ClientInstance::getLoopbackSender(void) -> LoopbackPacketSender* {
+
+	return *(LoopbackPacketSender**)((unsigned __int64)(this) + 0xE0);
+
+};
+
 auto ClientInstance::getMcGame(void) -> MinecraftGame* {
 
 	return *(MinecraftGame**)((unsigned __int64)(this) + 0xA8);
