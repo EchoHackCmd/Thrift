1e8e72a3c10645f86bb3096ddbf7c4c56a376fa8
EchoHackCmd <EchoHackCmd@gmail.com>
2022-11-02 07:14:34 +0000

    Update ClientInstance.h

--- Thrift/Client/SDK/Classes/ClientInstance.h
+++ Thrift/Client/SDK/Classes/ClientInstance.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include "../../Utils/Utils.h"
+
 #include "LoopbackPacketSender.h"
 #include "MinecraftGame.h"
 
 class ClientInstance {
 public:
-	auto getLoopbackSender(void)->LoopbackPacketSender*;
-	auto getMcGame(void)->MinecraftGame*;
+	BUILD_ACCESS(class LoopbackPacketSender*, LoopbackPacketSender, 0xE0);
+	BUILD_ACCESS(class MinecraftGame*, MinecraftGame, 0xA8);
 };
