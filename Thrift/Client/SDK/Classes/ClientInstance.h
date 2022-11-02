f3a9a5893d9c72d884c98552696aec82568aa458
EchoHackCmd <EchoHackCmd@gmail.com>
2022-11-02 07:37:31 +0000

    Update ClientInstance.h

--- Thrift/Client/SDK/Classes/ClientInstance.h
+++ Thrift/Client/SDK/Classes/ClientInstance.h
@@ -4,9 +4,20 @@
 
 #include "LoopbackPacketSender.h"
 #include "MinecraftGame.h"
+#include "Player.h"
 
 class ClientInstance {
+private:
+	uintptr_t** VTable;
+public:
+	auto getLocalPlayer(void) -> class Player* {
+		using GetLocalPlayer = Player * (__thiscall*)(ClientInstance*);
+		GetLocalPlayer _GetLocalPlayer = (GetLocalPlayer)(this->VTable[24]);
+		return _GetLocalPlayer(this);
+	};
 public:
 	BUILD_ACCESS(class LoopbackPacketSender*, LoopbackPacketSender, 0xE0);
 	BUILD_ACCESS(class MinecraftGame*, MinecraftGame, 0xA8);
+public:
+	AS_FIELD(class Player*, LocalPlayer, getLocalPlayer);
 };
