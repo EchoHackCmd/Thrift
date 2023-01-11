8fb156396f8134f8e2367f3dac068dd8fdc7392f
NRGJobro <85864573+NRGJobro@users.noreply.github.com>
2023-01-10 20:32:40 -0500

    Loopback Packet Sender

--- Thrift/Client/SDK/Classes/LoopbackPacketSender.h
+++ Thrift/Client/SDK/Classes/LoopbackPacketSender.h
@@ -4,5 +4,14 @@
 
 class LoopbackPacketSender {
 public:
-	//
+public:
+	virtual ~LoopbackPacketSender(); // constructor
+	virtual void send(void* Packet);
+	virtual void sendToServer(void* Packet);
+	virtual void sendToClient(class UserEntityIdentifierComponent const*, void* Packet);
+	virtual void sendToClient(class NetworkIdentifier const&, void* Packet, unsigned char);
+	virtual void sendToClients(std::vector<class NetworkIdentifierWithSubId, std::allocator<class NetworkIdentifierWithSubId>> const&, void* Packet);
+	virtual void sendBroadcast(void* Packet);
+	virtual void sendBroadcast(class NetworkIdentifier const&, unsigned char, void* Packet);
+	virtual void flush(class NetworkIdentifier const&);
 };
