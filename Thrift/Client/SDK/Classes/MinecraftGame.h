8c36115676da742003e6fc81c25fec1c3bb2a18a
NRGJobro <85864573+NRGJobro@users.noreply.github.com>
2023-01-10 20:37:03 -0500

    Font Class in MinecrafrtGame

--- Thrift/Client/SDK/Classes/MinecraftGame.h
+++ Thrift/Client/SDK/Classes/MinecraftGame.h
@@ -2,7 +2,18 @@
 
 #include "../../Utils/Utils.h"
 
-class Font;
+class Font {
+private:
+	virtual void constructor();
+	virtual void a1();
+	virtual void a2();
+	virtual void a3();
+	virtual void a4();
+	virtual void a5();
+public:
+	virtual float getLineLength(std::string* str, float textSize, bool unknown);
+	virtual float getLineHeight();
+};
 
 class MinecraftGame {
 public:
